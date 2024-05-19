035d7f7dc3d1a52f1602d877c598cd28e65e4c07
ClayGH <153925795+Clay2605293@users.noreply.github.com>
2024-05-18 18:45:59 -0600

    Add files via upload

---
+++ scripts.js
@@ -0,0 +1,6 @@
+document.addEventListener('DOMContentLoaded', (event) => {
+    const miBoton = document.getElementById('miBoton');
+    miBoton.addEventListener('click', () => {
+        alert('¡Botón clickeado!');
+    });
+});
